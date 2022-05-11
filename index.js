88f83af8815eee117512c4c4f047a13dd69238b1
revil-O <revil-O@users.noreply.github.com>
2022-05-11 16:13:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ SKODAConnect.prototype.getauth = function(callback) {
 				'Accept-Encoding':	'br, gzip, deflate',
 				'Content-Type' : 'application/x-www-form-urlencoded',
     		'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_1_1 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0 Mobile/15B150 Safari/604.1',
-				'Origin': 'https://customer.bmwgroup.com',
+				'Origin': 'https://customer.vwgroup.io',
 				//'Authorization': 'Basic ' + this.authbasic,
   			},
 				form: {
